f22ee5dd698737af25ebacd16da87b9925de8e28
martinbroede <martin.broede@yahoo.de>
2024-09-21 20:51:01 +0200

    rename static folder

--- django/demo/demo/settings.py
+++ django/demo/demo/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/v0/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'public'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
