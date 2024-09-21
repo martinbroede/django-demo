4629346b5b3b3672109a885b374826dfd1b0b449
martinbroede <martin.broede@yahoo.de>
2024-09-21 22:15:13 +0200

    update static url

--- django/demo/demo/settings.py
+++ django/demo/demo/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/v2/'
 STATIC_ROOT = BASE_DIR / 'public'
 
 # WhiteNoise configuration
