561b3662795299fd96478234dc3ce410b92d8c34
martinbroede <martin.broede@yahoo.de>
2024-09-21 21:17:23 +0200

    whitenoise attempt

--- django/demo/demo/wsgi.py
+++ django/demo/demo/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'public'))
