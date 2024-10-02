cbecfc025a8417bb842d80a93671d38857fe2d6d
martinbroede <martin.broede@gmail.com>
2024-10-02 11:36:24 +0200

    free CPU time

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,9 +1,16 @@
+from time import sleep
+
 from django.conf import settings
 from django.http import HttpResponse
 
 
-def example (request):
+def intensive_task():
     sum = 0
     for i in range(200_000_000):
+        if(i % 500_000 == 0):
+            sleep(0.05)
         sum += i
-    return HttpResponse(f"Version: {settings.VERSION} / {settings.REF} / Sum: {sum}")
+
+def example (request):
+    intensive_task()
+    return HttpResponse(f"Version: {settings.VERSION} / {settings.REF}")
